e38e6bdac56ecd4bd6e71212095c1398c200a417
shohanur00 <shohanur.waltonbd@gmail.com>
2026-03-08 00:19:05 +0600

    Uart Data Parse

--- Objects/main.d
+++ Objects/main.d
@@ -1,5 +1,5 @@
 ./objects/main.o: main.c \
-  C:\Users\WALTON\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f401xc.h \
-  C:\Users\WALTON\AppData\Local\Arm\Packs\ARM\CMSIS\6.2.0\CMSIS\Core\Include\core_cm4.h \
-  C:\Users\WALTON\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h \
+  C:\Users\user\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f401xc.h \
+  C:\Users\user\AppData\Local\Arm\Packs\ARM\CMSIS\6.2.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Users\user\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h \
   rcc.h gpio.h
